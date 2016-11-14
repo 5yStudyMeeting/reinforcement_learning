c11142a6eb76688edf89303555b6af6d468466a2
Kento Watanabe <k3nt0.watanabe@gmail.com>
2016-11-14 21:46:53 +0900

    価値関数plotの修正 & jupyterで可視化した

--- Section5/black_jack.py
+++ Section5/black_jack.py
@@ -2,6 +2,10 @@ import numpy as np
 from numpy.random import randint
 from copy import copy
 
+import numpy as np
+from numpy.random import randint
+from copy import copy
+
 class BlackJack(object):
     def __init__(self):
         self.upcard = None
@@ -18,6 +22,8 @@ class BlackJack(object):
         self.usable_ace_flag_of_dealer = False
         self.usable_ace_flag_of_player = False
 
+        self.change_flag = False
+
         self.HIT  = 0
         self.STAY = 1
         self.action = None
@@ -27,8 +33,7 @@ class BlackJack(object):
         self.DRAW   = 0
         self.winner = 0
 
-        self.state_info = None
-
+        self.state_info = []
         self.end_flag = False
 
     def drow(self):
@@ -47,6 +52,9 @@ class BlackJack(object):
         self.sum_of_player = np.sum(self.player)
         self._check_have_one()
 
+        self._judge_usable_ace(who="dealer")
+        self._judge_usable_ace(who="player")
+
         if self.having_ace_flag_of_dealer: self.sum_of_dealer += 10
         if self.having_ace_flag_of_player: self.sum_of_player += 10
 
@@ -59,8 +67,8 @@ class BlackJack(object):
             self._judge_usable_ace(who="player")
             if self.usable_ace_flag_of_player:
                 self.sum_of_player += 10
-            elif (not self.usable_ace_flag_of_player) \
-            and self.having_ace_flag_of_player:
+            if self.change_flag:
+                self.change_flag = False
                 self.sum_of_player -= 10
 
         elif who == "dealer":
@@ -70,25 +78,30 @@ class BlackJack(object):
             self._judge_usable_ace(who="dealer")
             if self.usable_ace_flag_of_dealer:
                 self.sum_of_dealer += 10
-            elif (not self.usable_ace_flag_of_dealer) \
-            and self.having_ace_flag_of_dealer:
+            if self.change_flag:
+                self.change_flag = False
                 self.sum_of_dealer -= 10
 
     def update(self):
         upcard = copy(self.upcard)
         sum_of_player = copy(self.sum_of_player)
+        usable = copy(self.usable_ace_flag_of_player)
         action = copy(self.action)
-        self.state_info = (upcard, sum_of_player, action)
+        self.state_info.append([upcard, sum_of_player, usable, action])
 
     def _dealer_drowing(self):
         while True:
             if self.sum_of_dealer > self.sum_of_player:
-                self.winner = self.DRAW
+                self.winner = self.DEALER
                 self.end_flag = True
                 break
             elif self.sum_of_dealer > 16:
                 if self.sum_of_dealer > self.sum_of_player:
                     self.winner = self.DEALER
+                    self.end_flag = True
+                    break
+                elif self.sum_of_dealer == self.sum_of_player:
+                    self.winner = self.DRAW
                     self.end_flag = True
                     break
                 else:
@@ -98,7 +111,7 @@ class BlackJack(object):
             else:
                 self.hit(who="dealer")
                 self._judge_bust(who="dealer")
-                if self.end_flag: return
+                if self.end_flag: break
 
     def _check_have_one(self):
         self.having_ace_flag_of_dealer = True \
@@ -109,15 +122,26 @@ class BlackJack(object):
     def _judge_usable_ace(self, who):
         #check whether the player can use the ace as ten.
         if who == "dealer":
-            self.usable_ace_flag_of_dealer = True \
-            if (1 in self.dealer) and self.sum_of_dealer < 11 else False
+            if self.usable_ace_flag_of_dealer:
+                if (1 in self.dealer) and self.sum_of_dealer < 11:
+                    self.usable_ace_flag_of_dealer = True
+                else:
+                    self.usable_ace_flag_of_dealer = False
+                    self.change_flag = True
+            else:
+                self.usable_ace_flag_of_dealer = True \
+                if (1 in self.dealer) and self.sum_of_dealer < 11 else False
+
         if who == "player":
             if self.usable_ace_flag_of_player:
                 if (1 in self.player) and self.sum_of_player < 11:
                     self.usable_ace_flag_of_player = True
                 else:
-                    self.usable_ace_flag_of_dealer:
-
+                    self.usable_ace_flag_of_player = False
+                    self.change_flag = True
+            else:
+                self.usable_ace_flag_of_player = True \
+                if (1 in self.player) and self.sum_of_player < 12 else False
 
     def _judge_bust(self, who="player"):
         if who == "player":
@@ -156,7 +180,6 @@ class BlackJack(object):
         print("Cards of dealer:", self.dealer)
         print("Winner:", self.winner)
         print("Have usable ace?:", self.usable_ace_flag_of_player)
-        print("")
         print("S_t-1 info:", self.state_info)
 
     def test(self):
@@ -165,5 +188,36 @@ class BlackJack(object):
         self.get_info()
 
 if __name__ == "__main__":
-    bj = BlackJack()
-    bj.test()
+    #STEP 1
+    # Calculate value function
+
+    # shape: (usable?, upcard, sum_of_player)
+    V = np.zeros((2,10,10))
+    N = np.zeros((2,10,10))
+
+    EPISODE = 10
+
+    for e in range(1,EPISODE+1):
+        bj = BlackJack()
+        bj.set()
+        """policy1
+        stay :  sum = 20, 21
+        hit  :  others
+        """
+        bj.play_with_policy1()
+        infos = bj.state_info
+        for info in infos:
+            upcard, sum_of_player, usable, _ = info[0], info[1], info[2], info[3]
+            #preprocess to fit shape
+            upcard -= 1
+            sum_of_player -= 12
+            usable = int(usable)
+
+            reward = bj.winner
+            #print(usable,upcard,sum_of_player,reward)
+            #print(N[usable,upcard,sum_of_player])
+
+            N[usable,sum_of_player,upcard] += 1
+            V[usable,sum_of_player,upcard] += \
+            (reward - V[usable,sum_of_player,upcard])\
+            /N[usable,sum_of_player,upcard]
